17c8290e446013d9c27bbcd34c5b0cf7566add2a
CardCaptor <marcelonallino@gmail.com>
2021-06-15 21:08:22 -0300

    Agregue navBar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+import {NavBar} from './components/navBar/navBar'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Aun no decido mi e-commerce
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBar/>
+      <h1>La Petaquita</h1>
     </div>
   );
 }
---
+++ src/components/navBar/navBar.js
@@ -0,0 +1,14 @@
+export const NavBar = () => {
+
+    return(
+        <nav>
+            <ul>
+               <li>Vinos</li>
+               <li>Espumantes </li>
+               <li>Cervezas</li>
+               <li>Whisky</li>
+               <li>Destilados</li>
+            </ul>
+        </nav>
+    )
+}
