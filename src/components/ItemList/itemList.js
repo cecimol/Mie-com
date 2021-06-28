9b9ff419983ad6d32075fcd24f59779c6f05e4b5
CardCaptor <marcelonallino@gmail.com>
2021-06-28 19:38:59 -0300

    Correcciones desafio

--- src/components/ItemList/itemList.js
+++ src/components/ItemList/itemList.js
@@ -1,73 +1,23 @@
-import {useEffect} from 'react'
 import {Item} from '../Item/item'
 import './itemList.css'
-import Campari from '../../img/Campari.png'
-import Branca from '../../img/Branca.jpg'
-import Trumpeter from '../../img/Trumpeter.png'
-import SantaJulia from '../../img/SantaJulia.png'
 
+export const ItemList = ({productos}) => {
 
-const PRODUCTOS = [
-    {
-        id:1,
-        title:'Campari',
-        price:550,
-        pictureURL:Campari
-    },
-    {
-        id:2,
-        title:'Branca',
-        price:650,
-        pictureURL:Branca
-    },
-    {
-        id:3,
-        title:'Trumpeter',
-        price:810,
-        pictureURL:Trumpeter
-    },
-    {
-        id:4,
-        title:'Santa Julia',
-        price:340,
-        pictureURL:SantaJulia
-    }
-]
-
-export const ItemList = () => {
-
-
-    useEffect(()=>{
-        
-        const obtenerProductos = new Promise((resolve, reject)=> {
-            
-           setTimeout(()=>{
-               resolve(PRODUCTOS)
-        
-           }, 2000)
-        })
-        
-        obtenerProductos.then(
-            (productos)=>{
-                console.log(productos)
-            }
-        )
-    },[])
-
-    return(<div className='productos'>
-        {PRODUCTOS.map((item, indice)=>{
-            return (
-                <Item
-                    key={item.id}
-                    id={item.id}
-                    title={item.title}
-                    price={item.price}
-                    pictureURL={item.pictureURL}
-                />
-            )
-        })}
-        
-    </div>)
+    return(
+        <div className='productos'>
+            {productos.map((item, indice)=>{
+                return (
+                    <Item
+                        key={item.id}
+                        id={item.id}
+                        title={item.title}
+                        price={item.price}
+                        pictureURL={item.pictureURL}
+                    />
+                )
+            })}       
+        </div>
+    )
 }
 
 
--- src/components/ItemListContainer/itemListContainer.js
+++ src/components/ItemListContainer/itemListContainer.js
@@ -1,8 +1,43 @@
-import {useState} from 'react'
+import {useState, useEffect} from 'react'
 import { ItemsCount } from '../ItemsCount/itemsCount'
 import {ItemList} from '../ItemList/itemList'
+import Campari from '../../img/Campari.png'
+import Branca from '../../img/Branca.jpg'
+import Trumpeter from '../../img/Trumpeter.png'
+import SantaJulia from '../../img/SantaJulia.png'
+
+const PRODUCTOS = [
+    {
+        id:1,
+        title:'Campari',
+        price:550,
+        pictureURL:Campari
+    },
+    {
+        id:2,
+        title:'Branca',
+        price:650,
+        pictureURL:Branca
+    },
+    {
+        id:3,
+        title:'Trumpeter',
+        price:810,
+        pictureURL:Trumpeter
+    },
+    {
+        id:4,
+        title:'Santa Julia',
+        price:340,
+        pictureURL:SantaJulia
+    }
+]
+
 export const ItemListContainer = () => {
+    
     const [stock, setStock] = useState(5)
+    const [productos, setProductos] = useState([])
+
     const handleAgregar = (number, setNumber) => {
         if (stock > 0){
             setNumber(number + 1)            
@@ -16,12 +51,25 @@ export const ItemListContainer = () => {
             setStock(stock+1)
         }
     }
-
+    
+    useEffect(()=>{        
+        const obtenerProductos = new Promise((resolve, reject)=> {     
+           setTimeout(()=>{
+               resolve(PRODUCTOS)               
+           }, 2000)
+        })
+        
+        obtenerProductos.then(
+            (productos)=>{
+                setProductos(productos)
+            }
+        )
+    },[])
     
 
     return(
         <div>
-        <ItemList/>
+        <ItemList productos={productos}/>
         <ItemsCount stock={stock} initial={1} onAdd={handleAgregar} onSubstract={handleResta}/>
         </div>
         )
